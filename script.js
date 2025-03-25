cef481690ffe22220b26fe5fff83f6fb3d9fe514
ArjunSingh1322 <as3388776@gmail.com>
2025-03-25 20:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let ul = document.getElementById("band");  
 
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled',
+    'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
 
-bands.sort((a,b)=>a-b)
-console.log(bands)
+bands.sort((a, b) => a.localeCompare(b));
+
+bands.forEach((band) => {
+    let li = document.createElement("li");
+    li.textContent = band;  
+    ul.appendChild(li);
+});
+
+console.log(bands);
